45f8440405c9ff19ec2b263ef6249a858dbde188
chaein <chaein@C010423195.zipangu.cac.co.jp>
2026-04-15 10:01:54 +0900

    lesson01

--- Java100_questions_cac2nd/src/lesson01/challenge09/Patisserie.java
+++ Java100_questions_cac2nd/src/lesson01/challenge09/Patisserie.java
@@ -42,6 +42,99 @@
 
 package lesson01.challenge09;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Patisserie {
+	public static void main(String[] args) throws IOException {
+
+		double num_siro = 30;
+		double num_choco = 30;
+		double num_pisu = 30;
+
+		System.out.println("本日のおすすめ商品です。");
+		System.out.println("");
+		System.out.println("シトロン　　　\\250・・・残り" + (int) num_siro + "個");
+		System.out.println("ショコラ　　　\\280・・・残り" + (int) num_choco + "個");
+		System.out.println("ピスターシュ　\\320・・・残り" + (int) num_pisu + "個");
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.println("それぞれ何個ずつ買いますか？（最大30個まで）");
+		System.out.println("");
+
+		System.out.print("シトロン　　　＞");
+		String str = reader.readLine();
+		double siro = Double.parseDouble(str);
+
+		System.out.print("ショコラ　　　＞");
+		str = reader.readLine();
+		double choco = Double.parseDouble(str);
+
+		System.out.print("ピスターシュ　＞");
+		str = reader.readLine();
+		double pisu = Double.parseDouble(str);
+
+		System.out.println();
+		System.out.println("シトロン　　" + siro + "個");
+		System.out.println("ショコラ　　" + choco + "個");
+		System.out.println("ピスターシュ" + pisu + "個");
+		System.out.println();
+
+		double cost_total = (siro * 250 + choco * 280 + pisu * 320);
+
+		System.out.println("合計個数　　" + (siro + choco + pisu) + "個です");
+		System.out.println("合計金額　" + (int) cost_total + "円です");
+
+		System.out.println("");
+		System.out.println("を買いあげですね。");
+		System.out.println("承りました。");
+
+		num_siro -= siro;
+		num_choco -= choco;
+		num_pisu -= pisu;
+
+		System.out.println("");
+		System.out.println("本日のおすすめ商品です。");
+		System.out.println("");
+		System.out.println("シトロン　　　\\250・・・残り" + (int) num_siro + "個");
+		System.out.println("ショコラ　　　\\280・・・残り" + (int) num_choco + "個");
+		System.out.println("ピスターシュ　\\320・・・残り" + (int) num_pisu + "個");
+
+		System.out.println("");
+		System.out.println("閉店時間となりました。");
+		System.out.println("またのお越しをお待ちしております。");
+		System.out.println("");
+
+		System.out.println("売上の割合");
+		System.out.println(" 売上合計 \\" + (int) cost_total);
+
+		System.out.println("");
+		System.out.println("内訳");
+
+		System.out.println("シトロン　　\\" + (int) (siro * 250) + "・・・" + (int) (siro * 250 / cost_total * 100) + "%");
+		System.out.println("ショコラ　　\\" + (int) (choco * 280) + "・・・" + (int) (choco * 280 / cost_total * 100) + "%");
+		System.out.println("ピスターシュ\\" + (int) (pisu * 320) + "・・・" + (int) (pisu * 320 / cost_total * 100) + "%");
+
+		System.out.println("");
+		System.out.println("明日の三色マカロンの配合率が決まりました！");
+
+		System.out.println("");
+		System.out.println("シトロン　　 " + "・・・" + (int) (siro * 250 / cost_total * 100) + "%");
+		System.out.println("ショコラ　　 " + "・・・" + (int) (choco * 280 / cost_total * 100) + "%");
+		System.out.println("ピスターシュ " + "・・・" + (int) (pisu * 320 / cost_total * 100) + "%");
+
+		System.out.println("");
+		System.out.println("が楽しめます！");
+
+		double total = siro + choco + pisu;
+		double cost_aver = cost_total / total;
+		cost_aver /= 10;
+
+		System.out.println("");
+		System.out.println("値段は\\" + (int) cost_aver * 10 + "です");
+
+	}
 
 }
